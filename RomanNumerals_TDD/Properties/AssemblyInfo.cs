bd9e846dee6fef1ebf1909ec1bd707a61faa8465
Nick Sittinger <nsittinger@gmail.com>
2017-11-15 15:14:33 -0500

    first commit - single digit test

---
+++ RomanNumerals_TDD/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RomanNumerals_TDD")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RomanNumerals_TDD")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f388e7d-88ba-428f-965c-0a418fb4e91c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RomanNumerals_TDD/RomanNumerals_UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals;
+
+namespace RomanNumerals_TDD
+{
+    [TestClass]
+    public class RomanNumerals_UnitTests
+    {
+
+        RomanNumeralModel romanNumerals = new RomanNumeralModel();
+
+        [TestMethod]
+        public void IfNumberIsOneDigitTest()
+        {
+            Assert.AreEqual("VIII", romanNumerals.ConvertSingleDigitToRomanNumerals(8));
+            Assert.AreEqual("V", romanNumerals.ConvertSingleDigitToRomanNumerals(5));
+            Assert.AreEqual("III", romanNumerals.ConvertSingleDigitToRomanNumerals(3));
+
+        }
+    }
+}
